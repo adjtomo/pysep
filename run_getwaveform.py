4f619363aeebc4e03d4ce76ddf18666e6284e609
Vipul Silwal <vipulsilwal@gmail.com>
2016-08-23 14:16:06 -0800

    add alaska exampl

--- run_getwaveform.py
+++ run_getwaveform.py
@@ -7,14 +7,50 @@ from obspy.clients.fdsn import Client
 
 c = Client("IRIS")
 
-otime = obspy.UTCDateTime("2016-01-24T10:30:29")
+# EXAMPLES:
+iex = 2
+
+# Iniskin earthquake
+if iex == 1:
+    otime = obspy.UTCDateTime("2016-01-24T10:30:29")
+    min_dist = 0 
+    max_dist = 500
+    before = 20
+    after = 600
+    network = 'AK,AT,YV,PS,AV,IU,II,XZ,XM'
+    channel = 'BH*'
+    samp_freq = 20.0
+    rotate = True
+    output_cap_weight_file = True
+    remove_response = True
+    detrend = True
+    demean = True
+    output_event_info = True
+    pre_filt = (0.005, 0.006, 5.0, 10.0)
+
+# SilwalTape2016 example event (Anchorage)
+if iex == 2:
+    otime = obspy.UTCDateTime("2009-04-07T20:12:55")
+    min_dist = 0 
+    max_dist = 300
+    before = 100
+    after = 300
+    network = 'AK,AT,YV,PS,AV,IU,II,XZ,XM'
+    channel = 'BH*'
+    samp_freq = 20.0
+    rotate = True
+    output_cap_weight_file = True
+    remove_response = True
+    detrend = True
+    demean = True
+    output_event_info = True
+    pre_filt = (0.005, 0.006, 5.0, 10.0)
 
 cat = c.get_events(starttime = otime-10, endtime = otime+10)
-network='AK,AT,YV,PS,AV,IU,II,XZ,XM'
-getwaveform_iris.run_get_waveform(ev=cat[0], min_dist=0, max_dist=500, 
-        before = 20, after = 600,
-        network=network, channel='BH*', samp_freq=20.0, rotate=True,
-        output_cap_weight_file=True, remove_response=True,
-        detrend=True, demean=True, output_event_info=True,
-                     pre_filt=(0.005, 0.006, 5.0, 10.0)
-                     )
+
+# Extract waveforms 
+getwaveform_iris.run_get_waveform(ev = cat[0], min_dist = min_dist, max_dist = max_dist, 
+        before = before, after = after, network = network, channel = channel, 
+        samp_freq = samp_freq, rotate = rotate,
+        output_cap_weight_file = output_cap_weight_file, remove_response = remove_response,
+        detrend = detrend, demean = demean, output_event_info = output_event_info, pre_filt = pre_filt)
