ea6ea16c4c958bd5705bdd89ed4102d7ce08c179
kslytherin <kksmith7@alaska.edu>
2016-08-23 13:52:37 -0800

    soh script

---
+++ get_soh.py
@@ -0,0 +1,119 @@
+#!/opt/antelope/python2.7.6/bin/python
+
+#import os
+#import sys
+
+#sys.path.append(os.environ['ANTELOPE'] + "/data/python")
+
+
+import obspy
+from obspy.core import *
+from obspy.clients.fdsn import Client
+import datetime
+from statistics import mode
+
+#import antelope.elog as elog
+
+def get_iris_soh(net, sta, cha, time1,time2):
+    client = Client("IRIS")
+    try:
+        st = client.get_waveforms(net, sta, "*", cha, time1, time2, attach_response = False)
+    except:
+        elog.die("No data available for station %s, cha %s. Exiting..." % (sta, cha))
+
+    # calculate statistics
+    samprates = []
+    dts = []
+    for ii in list(range(len(st))):
+        samprates.append(st[ii].stats.sampling_rate)
+        dts.append(st[ii].stats.delta)
+
+    mostpopsamp = mode(samprates)
+    mostpopdt = mode(dts)
+
+    # make mergeable
+    for ii in list(range(len(st))):
+        if st[ii].stats.sampling_rate != mostpopsamp:
+            pctoff = (mostpopsamp-st[ii].stats.sampling_rate)/mostpopsamp*100
+            if abs(pctoff) > 1:
+                print("sample rates off by ", pctoff, "%")
+            st[ii].stats.sampling_rate = mostpopsamp
+        if st[ii].stats.delta != mostpopdt:
+            st[ii].stats.delta = mostpopdt
+
+    st.merge(method=0)
+    tr = st[0]
+    sttime = tr.stats.starttime
+    fntime = tr.stats.endtime
+    numpoints = tr.stats.npts
+    date_list = [sttime + x*tr.stats.delta for x in range(0, numpoints)]
+    datetime_list = [date_list[y].datetime for y in range(0, numpoints)]
+    tt = datetime_list
+    return tr,tt
+
+
+#try:
+
+#except:
+#    sys.exit("Problem importing elog library.  Check your Antelope installation.  Exiting...")
+
+# Datascope libraries
+#import antelope.datascope as datascope
+
+# Stock libraries
+#import antelope.stock as stock
+
+
+def get_aec_soh(sta, cha, yea, mon, day,hou, min, sec):
+    
+    # Read from state of health directory
+    db_nme = '/aerun/op/db/diagnostic/diagnostic'
+    # OR
+    #db_nme = '/aec/db/diagnostics/diagnostics'
+    #try:
+    db = datascope.dbopen(db_nme, 'r')
+    #except:
+    #    elog.die("Problem loading the database [%s] for processing!  Exiting..." % db_nme)
+
+    db = db.lookup(table='wfdisc')
+
+    # The origin time
+    t_or = "%s/%s/%s %s:%s:%s.00" % (mon, day, yea, hou, min, sec)
+
+    # Convert to epoch
+    t_or_st = stock.str2epoch(t_or)
+
+    # Window around the origin time.  Time added is in units of seconds
+    t1 = t_or_st + 0
+    t2 = t_or_st + 60*60 
+
+    # Load the trace
+    tr = db.trloadchan(t1, t2, sta, cha)
+
+    # Get the required data from the trace
+    tr.record = 0
+    (nsamp, samprate) = tr.getv('nsamp', 'samprate')
+    d = array(tr.trdata()) # The actual data
+
+    # Sanity check
+    if (nsamp != len(d)):
+        elog.die("For some reason the data vector has a length that does not match the number of samples!  Exiting...")
+
+    # Convert trace object to ObsPy
+    st = Stream() # Empty stream
+    tr0 = Trace() # Empty ObsPy trace object 
+
+    # Populate the trace object
+    tr0.stats.network = net
+    tr0.stats.station = sta
+    tr0.stats.channel = cha
+    tr0.stats.sampling_rate = samprate
+    tr0.stats.npts = nsamp
+    tr0.stats.starttime = t1
+    tr0.stats.starttime = t2
+    tr0.data = d
+
+    # Append the stream with the new trace object
+    st.append(tr0)
+
+    return st
---
+++ run_get_soh.py
@@ -0,0 +1,117 @@
+#!/opt/antelope/python2.7.6/bin/python
+# units for state of health can be found here:
+# https://www.passcal.nmt.edu/content/data-archiving/documentation/passive-source
+
+#from obspy.core import *
+#from obspy.clients.fdsn import Client
+#
+from get_soh import get_iris_soh
+from get_soh import get_aec_soh
+import matplotlib.pyplot as plt
+import numpy
+import matplotlib.dates as dates
+import datetime
+import obspy
+from obspy.core import *
+
+import os
+import sys
+
+sys.path.append(os.environ['ANTELOPE'] + "/data/python")
+
+# Set parameters
+net = "XV"
+sta = "FTGH"
+
+time1 = UTCDateTime(2015,8,1,0,0,0)
+time2 = UTCDateTime(2015,9,1,0,0,0)
+
+
+# get data
+vm0,vm0t = get_iris_soh(net,sta,"VM0",time1,time2)
+
+vm1,vm1t = get_iris_soh(net,sta,"VM1",time1,time2)
+
+vm2,vm2t = get_iris_soh(net,sta,"VM2",time1,time2)
+
+vki,vkit = get_iris_soh(net,sta,"VKI",time1,time2)
+
+veptemp,vept = get_iris_soh(net,sta,"VEP",time1,time2)
+vep = [a*0.15 for a in veptemp]
+
+vec,vect = get_iris_soh(net,sta,"VEC",time1,time2)
+
+# make plots
+fig = plt.figure(num=1,figsize=(8.5,11))
+plt.subplot(411)
+plt.title(sta + ' ' + vkit[0].strftime('%Y/%m/%d') + ' - ' + vkit[len(vkit)-1].strftime('%Y/%m/%d') + "\n VKI temperature ")
+plt.plot_date(vkit,vki,'b-')
+plt.ylim(-10,50)
+plt.ylabel("Celsius")
+
+plt.subplot(412)
+plt.title("VEP main system voltage")
+plt.plot_date(vept,vep,'b-')
+plt.ylim(8,17)
+plt.ylabel("Volts")
+
+plt.subplot(413)
+plt.title("VEC main system current")
+plt.plot_date(vect,vec,'b-')
+plt.ylim(50,100)
+plt.ylabel("mA")
+
+plt.subplot(414)
+plt.title("Mass Positions")
+plt.plot_date(vm0t,vm0,'g-',label="VM0")
+plt.plot_date(vm1t,vm1,'r-',label="VM1")
+plt.plot_date(vm2t,vm2,'b-',label="VM2")
+plt.ylim(-35,35)
+plt.legend(loc=2,fontsize = 10)
+
+#pp = max(abs(i) for i in vm0)
+#pp2 = max(abs(j) for j in vm1)
+#pp3 = max(abs(k) for k in vm2)
+#ymax = max(pp,pp2,pp3)
+
+#plt.ylim(-ymax,ymax)
+
+fig.autofmt_xdate()
+fig.savefig(sta + "soh.pdf", dpi=200)
+
+plt.show()
+
+'''
+
+cha = "VM0"
+
+vmo_aec = get_aec_soh(sta,cha,2016,8,1,0,0,0)
+'''
+
+'''
+# get groundmotion waveforms
+da2 = 17
+ho2 = 11
+mi2 = 13
+oadd = 60*60
+w1,t1 = get_iris_soh(net,sta,"HHZ",ye,mo,da2,ho2,mi2,se,oadd)
+
+w2,t2 = get_iris_soh(net,sta,"HHN",ye,mo,da2,ho2,mi2,se,oadd)
+
+w3,t3 = get_iris_soh(net,sta,"HHE",ye,mo,da2,ho2,mi2,se,oadd)
+
+fig = plt.figure(3)
+plt.subplot(311)
+plt.title(sta + " HHZ")
+plt.plot_date(t1,w1,'b-')
+plt.subplot(312)
+plt.title("HHN")
+plt.plot_date(t2,w2,'b-')
+plt.subplot(313)
+plt.title("HHE")
+plt.plot_date(t3,w3,'b-')
+fig.autofmt_xdate()
+fig.savefig(sta + "wavesignal.png")
+
+plt.show()
+'''
