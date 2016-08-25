e7b916c34d0d40579c3538bdf2032bc5545a81c4
Carl Tape <carltape@gmail.com>
2016-08-25 09:32:14 -0800

    create idb and default settings

--- run_getwaveform.py
+++ run_getwaveform.py
@@ -1,14 +1,21 @@
 # To run this script:
 # > python run_getwaveform.py
 import obspy
-import getwaveform_iris
 import copy
-from obspy.clients.fdsn import Client
-
-c = Client("IRIS")
 
 # EXAMPLES:
 iex = 2
+idb = 1    # default: =1-IRIS, =2-AEC, =3-LLNL
+
+# default settings
+rotate = True
+output_cap_weight_file = True
+remove_response = True
+detrend = True
+demean = True
+output_event_info = True
+pre_filt = (0.005, 0.006, 5.0, 10.0)
+samp_freq = 20.0
 
 # Iniskin earthquake
 # This produces NO waveforms and exits with this error:
@@ -20,16 +27,9 @@ if iex == 1:
     #before = 42   # Crashes
     before = 41    # works fine
     after = 600
+    # Vipul: figure out which network it is
     network = 'AK,AT,AV,CN,II,IU,TA,XM,XV,XZ,ZE'
     channel = 'BH*'
-    samp_freq = 20.0
-    rotate = True
-    output_cap_weight_file = True
-    remove_response = True
-    detrend = True
-    demean = True
-    output_event_info = True
-    pre_filt = (0.005, 0.006, 5.0, 10.0)
 
 # SilwalTape2016 example event (Anchorage)
 if iex == 2:
@@ -40,14 +40,6 @@ if iex == 2:
     after = 300
     network = 'AK,AT,YV,PS,AV,IU,II,XZ,XM'
     channel = 'BH*'
-    samp_freq = 20.0
-    rotate = True
-    output_cap_weight_file = True
-    remove_response = True
-    detrend = True
-    demean = True
-    output_event_info = True
-    pre_filt = (0.005, 0.006, 5.0, 10.0)
 
 # Example for showing the script crashes when requesting for IM network stations
 # NotImplementedError: ResponseListResponseStage not yet implemented due to missing example data. Please contact the developers with a test data set (waveforms and StationXML metadata).
@@ -60,14 +52,6 @@ if iex == 3:
     #network = 'AK,IM' # Crashes (Error because of the IM network)
     network = 'AK'   # works fine 
     channel = 'BH*'
-    samp_freq = 20.0
-    rotate = True
-    output_cap_weight_file = True
-    remove_response = True
-    detrend = True
-    demean = True
-    output_event_info = True
-    pre_filt = (0.005, 0.006, 5.0, 10.0)
 
 # Iniskin earthquake -- trying to get waveforms from ZE (SALMON), which is embargoed
 if iex == 4:
@@ -78,14 +62,6 @@ if iex == 4:
     after = 600
     network = 'AK,ZE'   # no ZE waveforms returned
     channel = '*H*'
-    samp_freq = 20.0
-    rotate = True
-    output_cap_weight_file = True
-    remove_response = True
-    detrend = True
-    demean = True
-    output_event_info = True
-    pre_filt = (0.005, 0.006, 5.0, 10.0)
 
 # Iniskin earthquake -- trying to get KDAK waveforms (different location codes)
 # note that we should have B = -100 but that is not the case
@@ -97,19 +73,19 @@ if iex == 5:
     after = 600
     network = 'II'
     channel = 'BH*'
-    samp_freq = 20.0
-    rotate = True
-    output_cap_weight_file = True
-    remove_response = True
-    detrend = True
-    demean = True
-    output_event_info = True
-    pre_filt = (0.005, 0.006, 5.0, 10.0)
 
-cat = c.get_events(starttime = otime-10, endtime = otime+10)
+# fetch and process waveforms
+if idb == 1:
+    # import functions to access waveforms
+    import getwaveform_iris
+    from obspy.clients.fdsn import Client
+    c = Client("IRIS")
+    # will only work for events in the 'IRIS' catalog
+    # (future: for Alaska events, read the AEC catalog)
+    cat = c.get_events(starttime = otime-10, endtime = otime+10)
 
-# Extract waveforms 
-getwaveform_iris.run_get_waveform(ev = cat[0], min_dist = min_dist, max_dist = max_dist, 
+    # Extract waveforms 
+    getwaveform_iris.run_get_waveform(ev = cat[0], min_dist = min_dist, max_dist = max_dist, 
         before = before, after = after, network = network, channel = channel, 
         samp_freq = samp_freq, rotate = rotate,
         output_cap_weight_file = output_cap_weight_file, remove_response = remove_response,
