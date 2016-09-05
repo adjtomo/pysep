ac84b242d31f8b896c98180e29fe681fc8f4c47b
cralvizuri <cralvizuri@alaska.edu>
2016-09-05 12:57:11 -0800

    add back cut option

--- getwaveform_llnl.py
+++ getwaveform_llnl.py
@@ -13,8 +13,8 @@ from scipy import signal
 
 from util_write_cap import *
 
-#def resample(st, freq, starttime, npts):
-def resample(st, freq):
+#def resample(st, freq):
+def resample(st, freq, starttime, npts):
     """
     Custom resampling with a very sharp zerophase filter.
     """
@@ -28,10 +28,10 @@ def resample(st, freq):
         tr.taper(max_percentage=0.02, type="hann")
         tr.data = np.require(tr.data, requirements=["C"])
         # "Perfect" sinc resampling.
-        #tr.interpolate(sampling_rate=freq, method="lanczos", a=8,
-        #               window="blackman", starttime = starttime, npts = npts)
         tr.interpolate(sampling_rate=freq, method="lanczos", a=8,
-                       window="blackman")
+                       window="blackman", starttime = starttime, npts = npts)
+        #tr.interpolate(sampling_rate=freq, method="lanczos", a=8,
+        #               window="blackman")
 
 def run_get_waveform(llnl_db_client, event, 
                      min_dist=20, max_dist=300, before=100, after=300, 
@@ -179,12 +179,13 @@ def run_get_waveform(llnl_db_client, event,
 
     if resample_freq != 0:
         print("\n--> !! WARNING -- Resampling !!")
-        print("--> New sample rate = %5.1f\n" % resample_freq)
-        #npts = (after - before) * resample_freq
-        #t1 = evtime - before
-        print("%s\n%s" % (evtime, evtime - before))
-        #resample(st2, freq=resample_freq, starttime = t1, npts = npts)
-        resample(st2, freq=resample_freq)
+        print("--> New sample rate = %5.1f" % resample_freq)
+        npts = (after - before) * resample_freq
+        t1 = evtime - before
+        print("--> %s <-- t0" % (evtime))
+        print("--> %s <-- t0 - before_t0\n" % (evtime - before))
+        resample(st2, freq = resample_freq, starttime = t1, npts = npts)
+        #resample(st2, freq=resample_freq)
 
     write_stream_sac(st2, evtime)
 
