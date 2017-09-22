7753eb24b3ed3703219b3a77bd0e817a69e690de
Carl Tape <carltape@gmail.com>
2017-09-21 18:04:48 -0800

    tomoDD SALMON (iex=10)

--- event_input.py
+++ event_input.py
@@ -157,7 +157,7 @@ def get_ev_info(ev_info,iex):
             ev_info_temp.tbefore_sec = 100
             ev_info_temp.tafter_sec = 500
             ev_info_temp.network = 'AV,CN,ZE,AT,TA,AK,XV,II,IU,US' 
-            #ev_info.network = 'AK,AT,AV,CN,II,IU,US,XM,TA,XE,XR,XZ,YV,XV,ZE,XG'
+            #ev_info_temp.network = 'AK,AT,AV,CN,II,IU,US,XM,TA,XE,XR,XZ,YV,XV,ZE,XG'
             ev_info_temp.channel = 'BH?,HH?'
             ev_info_temp.resample_freq = 40
             ev_info_temp.scale_factor = 100
@@ -285,11 +285,35 @@ def get_ev_info(ev_info,iex):
         ev_info.idb = 1
         ev_info.overwrite_ddir = 1       # delete data dir if it exists
         ev_info.use_catalog = 0          # do not use event catalog for source parameters
+        ev_info.resample_TF = False
+        ev_info.scale_factor = 1         # no scale factor
+        ev_info.output_cap_weight_file = False
+        ev_info.ifsave_stationxml = False
+
         events_file = "/home/carltape/PROJECTS/SALMON/data/salmon_tomodd_obspy_sub.txt"
         eids,otimes,elons,elats,edeps,emags = reof.read_events_obspy_file(events_file)
+        
+        ev_info.ifFilter = True
+        ev_info.filter_type = 'highpass'
+        ev_info.f1 = 1.0
+
+        # subset of stations
+        #ev_info.min_dist = 0
+        #ev_info.max_dist = 200
+        ev_info.min_lat = 59
+        ev_info.max_lat = 62
+        ev_info.min_lon = -156
+        ev_info.max_lon = -148
+
+        ev_info.tbefore_sec = 20
+        ev_info.tafter_sec = 140
+        ev_info.network = 'AK,AT,AV,CN,II,IU,US,XM,TA,XE,XR,XZ,YV,XV,ZE,XG'
+        #ev_info.network = "AK"
+        ev_info.channel = 'BH?,HH?'
 
         ev_info_list = []
-        for xx in range(8,9):
+        #for xx in range(5,10):
+        for xx in range(len(eids)):
             ev_info_temp = ev_info.copy()
             ev_info_temp.otime = obspy.UTCDateTime(otimes[xx])
             ev_info_temp.elat = elats[xx]
@@ -297,16 +321,6 @@ def get_ev_info(ev_info,iex):
             ev_info_temp.edep = edeps[xx]
             ev_info_temp.emag = emags[xx]
             ev_info_temp.eid = eids[xx]
-            
-            # subset of stations
-            ev_info_temp.min_dist = 0
-            ev_info_temp.max_dist = 200
-            ev_info_temp.tbefore_sec = 20
-            ev_info_temp.tafter_sec = 200
-            #ev_info.network = 'AK,AT,AV,CN,II,IU,US,XM,TA,XE,XR,XZ,YV,XV,ZE,XG'
-            ev_info_temp.network = "AK"
-            ev_info_temp.channel = 'BH?,HH?'
-
             # append getwaveform objects
             ev_info_list.append(ev_info_temp)
         
