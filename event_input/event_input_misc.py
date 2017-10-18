335f7b7251d749dac0d2a07ecf2c15020255b32a
Vipul Silwal <vipulsilwal@gmail.com>
2017-10-17 16:55:36 -0800

    add Icy Bay earthquake

--- event_input/event_input_misc.py
+++ event_input/event_input_misc.py
@@ -56,10 +56,33 @@ def get_ev_info(ev_info,iex):
         ev_info.max_dist = 1000
         ev_info.tbefore_sec = 300
         ev_info.tafter_sec = 500
-        ev_info.network = 'XF'
+        ev_info.network = 'II'
         ev_info.channel = 'LH?,BH?'
         ev_info.resample_freq = 50        # no resampling
         ev_info.scale_factor = 100         # no scale factor
+
+# Icy Bay M3.0
+    if iex == 2:
+        ev_info.idb = 1
+        ev_info.overwrite_ddir = 1       # delete data dir if it exists
+        ev_info.use_catalog = 0          # do not use event catalog for source parameters
+        # GCMT source parameters
+        # the otime is the centroid time and accounts for tshift
+        ev_info.otime = obspy.UTCDateTime("2017-10-16T22:00:31") 
+        ev_info.elat = 57.6160
+        ev_info.elon = -142.7699
+        ev_info.edep = 6000
+        ev_info.emag = 3.0
+        # subset of stations
+        ev_info.min_dist = 0
+        ev_info.max_dist = 500
+        ev_info.tbefore_sec = 50
+        ev_info.tafter_sec = 200
+        ev_info.network = 'AK'
+        ev_info.channel = 'BH?'
+        ev_info.resample_freq = 50        # no resampling
+        ev_info.scale_factor = 100        # no scale factor
+
 
     return(ev_info)
 #=================================================================================
