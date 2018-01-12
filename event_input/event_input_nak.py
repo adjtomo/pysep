41b578e7c042958225c051e3bf959abf858875a6
Carl Tape <carltape@gmail.com>
2018-01-12 12:03:34 -0900

    reduce to 300 km station distances; remove AV

--- event_input/event_input_nak.py
+++ event_input/event_input_nak.py
@@ -12,23 +12,21 @@ def get_ev_info(ev_info,iex):
 
         # subset waveforms
         ev_info.min_dist = 0 
-        ev_info.max_dist = 500
+        ev_info.max_dist = 300
         ev_info.tbefore_sec = 50
         ev_info.tafter_sec = 300
         ev_info.resample_freq = 50
         ev_info.scale_factor = 100
-        ev_info.network = 'AK,AT,AV,CN,II,IU,US,XM,TA,XE,XR,XZ,YV,XV,ZE,XG'
+        ev_info.network = 'AK,AT,CN,II,IU,US,XM,TA,XE,XR,XZ,YV,XV,ZE,XG'
         ev_info.channel = 'BH?,HH?'
         
         # events frmo get_events_nak.m
         #events_file = "./test_data/nak_obspy.txt"
-        #ievent = 8
+        #ievent = 6
         # three recent events
-        events_file = "./test_data/nak_supp_obspy.txt"
-        ievent = 2   # 1:Manley, 2:Healy, 3:Canada
+        #events_file = "./test_data/nak_supp_obspy.txt"
+        ievent = 2               # 1:Manley, 2:Healy, 3:Canada
         
-        eids,otimes,elons,elats,edeps,emags = reof.read_events_obspy_file(events_file)
-
         ev_info_list = []
         for xx in range(ievent-1,ievent):
             ev_info_temp = ev_info.copy()
