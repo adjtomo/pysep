3a071a548eacd7096937930b0df3671314ad1931
kslytherin <kksmith7@alaska.edu>
2018-11-30 10:59:41 -0900

    anchorage WFs

--- event_input/event_input_scak.py
+++ event_input/event_input_scak.py
@@ -292,6 +292,32 @@ def get_ev_info(ev_info,iex):
         #ev_info.phases = ["P","P"]
         ev_info.write_sac_phase = True
         ev_info.taupmodel = "ak_scak" 
+
+    if iex == 12:
+        ev_info.idb = 1
+        ev_info.overwrite_ddir = 1       # delete data dir if it exists
+        ev_info.use_catalog = 0          # do not use event catalog for source parameters
+        # GCMT source parameters
+        # the otime is the centroid time and accounts for tshift
+        ev_info.otime = obspy.UTCDateTime("2018-11-30T17:29:29.000") 
+        ev_info.elat = 61.3234
+        ev_info.elon = -149.9234
+        ev_info.edep = 43000 
+        ev_info.emag = 7.0
+    
+        # subset of stations
+
+        # ev_info.min_dist = 0
+        ev_info.min_dist = 300
+        ev_info.max_dist = 500 
+        ev_info.tbefore_sec = 100 
+        ev_info.tafter_sec = 1000 
+        ev_info.network = 'AV,CN,ZE,AT,TA,AK,XV,II,IU,US' 
+        ev_info.channel = 'BH?,HH?'
+        ev_info.resample_freq = 50        # no resampling
+        ev_info.scale_factor = 100         # no scale factor
+        ev_info.user = ''
+        ev_info.password = ''
     return(ev_info)
 #=================================================================================
 # END EXAMPLES
