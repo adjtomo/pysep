e8c5db24d314c23015dfa329fee4a22cc9d27f82
kslytherin <kksmith7@alaska.edu>
2019-01-17 16:45:17 -0900

    new Tanana event

--- event_input/event_input_tact.py
+++ event_input/event_input_tact.py
@@ -69,7 +69,6 @@ def get_ev_info(ev_info,iex):
         ev_info.resample_freq = 50        # no resampling
         ev_info.scale_factor = 100         # no scale factor
 
-# December 30, 2018 Healy earthquake M4.1
     if iex == 3:
         ev_info.idb = 1
         ev_info.overwrite_ddir = 1       # delete data dir if it exists
@@ -88,6 +87,33 @@ def get_ev_info(ev_info,iex):
         ev_info.tbefore_sec = 100
         ev_info.tafter_sec = 500
         ev_info.network = 'AV,CN,ZE,AT,TA,AK,XV,II,IU,US' 
+        ev_info.channel = 'BH?,HH?'
+        ev_info.resample_freq = 50        # no resampling
+        ev_info.scale_factor = 100         # no scale factor        
+   
+    # M5.1 W. of Tanana 
+    if iex == 4:
+        ev_info.idb = 1
+        ev_info.overwrite_ddir = 1       # delete data dir if it exists
+        ev_info.use_catalog = 0          # do not use event catalog for source parameters
+        # GCMT source parameters
+        # the otime is the centroid time and accounts for tshift
+        ev_info.otime = obspy.UTCDateTime("2019-01-06T03:45:34") # AEC prelim
+        ev_info.elat = 65.4070
+        ev_info.elon = -153.2799
+        ev_info.edep = 17000
+        ev_info.emag = 5.1 
+    
+        # subset of stations
+        ev_info.min_dist = 0
+        ev_info.max_dist = 300
+        ev_info.tbefore_sec = 100
+        ev_info.tafter_sec = 500
+
+        # For DE (Nanometrics) data
+        ev_info.user = 'kksmith7@alaska.edu' 
+        ev_info.password = 'wmpo3NmqTcRm' 
+        ev_info.network = 'AV,CN,ZE,AT,TA,AK,XV,II,IU,US,DE' 
         ev_info.channel = 'BH?,HH?'
         ev_info.resample_freq = 50        # no resampling
         ev_info.scale_factor = 100         # no scale factor        
