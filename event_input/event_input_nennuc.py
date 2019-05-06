83c833a4488edd05766b55f41b242011628a9902
Carl Tape <carltape@gmail.com>
2019-05-05 22:41:40 -0800

    update nennuc input

--- event_input/event_input_nennuc.py
+++ event_input/event_input_nennuc.py
@@ -4,8 +4,9 @@ from getwaveform import *
 
 def get_ev_info(ev_info,iex):
 # ===============================================================
+# Vipul Silwal: 6 moment tensors in Tape et al. (2018 Nature Geoscience); 
+# python run_getwaveform.py event_input_nennuc 0
     if iex == 0:
-        ev_info.idb = 1
         ev_info.use_catalog = 0
         # -------------------------------------------------
         # VLFE + EQ (from 2012 Trigger paper)
@@ -13,43 +14,45 @@ def get_ev_info(ev_info,iex):
         #ev_info.otime = obspy.UTCDateTime("2012-04-11T09:21:57.444")
         #ev_info.elat = 64.9222
         #ev_info.elon = -148.9461
-        #ev_info.edep = 19343.1
-        #ev_info.emag = 3.93
+        #ev_info.edep = 16000
+        #ev_info.emag = 3.80
         # -------------------------------------------------
         # VLFE
         #ev_info.otime = obspy.UTCDateTime("2013-03-12T07:39:50.214")
         #ev_info.elat = 64.7161
         #ev_info.elon = -148.9505
-        #ev_info.edep = 20000
-        #ev_info.emag = 2.1
+        #ev_info.edep = 23000
+        #ev_info.emag = 3.5
         # -------------------------------------------------
         # VLFE
         #ev_info.otime = obspy.UTCDateTime("2015-09-12T03:25:12.711")
         #ev_info.elat = 65.1207
         #ev_info.elon = -148.6646
-        #ev_info.edep = 15556.8
-        #ev_info.emag = 2.63
+        #ev_info.edep = 21000
+        #ev_info.emag = 3.80
         # -------------------------------------------------
         # EQ
         #ev_info.otime = obspy.UTCDateTime("2015-10-22T13:16:15.794")
         #ev_info.elat = 64.7334
         #ev_info.elon = -149.0388
-        #ev_info.edep = 18830.20
-        #ev_info.emag = 2.9
+        #ev_info.edep = 18000
+        #ev_info.emag = 2.6
         # -------------------------------------------------
         # EQ
         #ev_info.otime = obspy.UTCDateTime("2015-10-31T02:56:35.572")
         #ev_info.elat = 64.4285
         #ev_info.elon = -149.6969
-        #ev_info.edep = 23852.1
-        #ev_info.emag = 3.47
+        #ev_info.edep = 25000
+        #ev_info.emag = 3.40
         # -------------------------------------------------
         # VLFE + EQ
         ev_info.otime = obspy.UTCDateTime("2016-01-14T19:04:10.727")
         ev_info.elat = 64.6827
         ev_info.elon = -149.2479
-        ev_info.edep = 22663.7
-        ev_info.emag = 3.80
+        ev_info.edep = 17000
+        ev_info.emag = 3.70
+        # Output pole-zero file (Chen Ji needed this for source estimation)
+        #ev_info.ifsave_sacpaz = True
         # -------------------------------------------------
         
         ev_info.min_dist = 0
@@ -60,9 +63,7 @@ def get_ev_info(ev_info,iex):
         # ev_info.network = 'XV,AK'
         ev_info.channel = 'BH?,HH?'
         # for CAP
-        ev_info.scale_factor = 10.0**2
+        ev_info.scale_factor = 100
         ev_info.resample_freq = 50
-        # Output pole-zero file (Chen Ji needed this for source estimation)
-        ev_info.ifsave_sacpaz = True
 
     return(ev_info)
