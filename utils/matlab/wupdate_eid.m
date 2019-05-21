9cf232831cad385e4c1ffd52f82c782abb10a1c4
Carl Tape <carltape@gmail.com>
2019-05-20 18:32:04 -0800

    fix for loading a single sac file

--- utils/matlab/wupdate_eid.m
+++ utils/matlab/wupdate_eid.m
@@ -4,7 +4,7 @@ function w = wupdate_eid(w)
 % This script should be removed once we fix the issue in run_getwaveform.py
 %
 % called by loadsac_all.m
-% Carl Tape, 2016/11/11
+% Carl Tape, 2016-11-11
 %
 
 warning('adding a missing digit to the event id (KEVNM)');
@@ -16,6 +16,11 @@ n = length(w);
 
 eid2 = eid1;
 for ii=1:n
-   eid2(ii) = {sprintf('%s%s',eid1{ii},num2str(mod(msec(ii),10)))};
-   w(ii) = set(w(ii),'KEVNM',eid2{ii});
+    xchar = num2str(mod(msec(ii),10));
+    if n==1
+        eid2 = {sprintf('%s%s',eid1,xchar)};
+    else
+        eid2(ii) = {sprintf('%s%s',eid1{ii},xchar)};
+    end
+    w(ii) = set(w(ii),'KEVNM',eid2{ii});
 end
