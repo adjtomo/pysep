fb57cf00f2c337ac8e3c538a07a62f3c8f7c2ac2
kslytherin <kksmith7@alaska.edu>
2019-07-19 10:17:41 -0800

    option to read all stations in file

--- read_station_file.py
+++ read_station_file.py
@@ -3,13 +3,20 @@
 
 def read_station_file(filename):
     f = open(filename, "r")
-    lines = f.readlines()
+    lines = f.readlines()[1:]
     f.close()
     netwk = [];stn_name = [];
-
+    # Before 2019/7/17 it was "True" but now it will be False 
+    # since we do not want to redownload WFs everytime stations 
+    # selection changes, just use matlab, etc to omit stations
+    use_ones_stations = False; 
     for line in lines:
         line_elements = line.split()
-        if line_elements[7] == '1':
+        if (line_elements[7] == '1') and use_ones_stations:
             netwk.append(line_elements[0])
             stn_name.append(line_elements[1])
+        elif not use_ones_stations:
+            netwk.append(line_elements[0])
+            stn_name.append(line_elements[1])
+    print(stn_name)
     return netwk,stn_name
