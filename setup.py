2d65f7bb82a13a696fb24a624b81a6fdb674759d
Bryant Chow <bryant.h.c@gmail.com>
2022-05-20 09:51:19 -0800

    [setup] updating package install requirements to include Lion Krischer's llnl_db_client directly from github

--- setup.py
+++ setup.py
@@ -2,6 +2,13 @@ import os
 from setuptools import setup, find_packages
 
 
+install_requires = [
+        "obspy>=1.2.2", 
+        "cartopy>=0.20.2", 
+        "pyyaml>=5.4",
+        "llnl_db_client @ git+https://github.com/krischer/llnl_db_client.git",
+        ]
+
 setup(name="pysep",
       version='0.1.0',
       description="Python Seismogram Extraction and Processing",
@@ -10,6 +17,6 @@ setup(name="pysep",
       license='GPL-3.0',
       python_requires=">=3.7",
       packages=find_packages(),
-      install_requires=["obspy", "cartopy", "pyyaml"],
+      install_requires=[
       zip_safe=False
       )
