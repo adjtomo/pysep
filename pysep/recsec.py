BUGFIX: RecordSection was incorrectly using indices in units of time, and not samples, to index the visible portion of the waveform for use in determining the local max amplitude within the time window. This would have led to incorrectly scaled waveforms when using the option 'xlim_s'. This bugfix sets the correct index unit, and ensures that all indices coming out of getter function 'get_xlim' are integers so that they directly be used to index waveform data

pysep/recsec.py
@@ -841,24 +841,17 @@ class RecordSection:
         # Max amplitudes should be RELATIVE to what were showing (e.g., if
         # zoomed in on the P-wave, max amplitude should NOT be the surface wave)
         for tr, xlim in zip(self.st, self.xlim):
-            start, stop = xlim
-            if start:
-                start = int(start * tr.stats.sampling_rate)
-            if stop:
-                stop = int(stop * tr.stats.sampling_rate)
+            start, stop = xlim  # units: samples
             self.max_amplitudes = np.append(self.max_amplitudes,
                                             max(abs(tr.data[start:stop])))
+
         self.max_amplitudes = np.array(self.max_amplitudes)
 
         # Max amplitudes will be DIFFERENT for synthetics, which affects 
         # normalizations and thus requires its own array
         if self.st_syn is not None:
             for tr, xlim in zip(self.st_syn, self.xlim):
-                start, stop = xlim
-                if start:
-                    start = int(start * tr.stats.sampling_rate)
-                if stop:
-                    stop = int(stop * tr.stats.sampling_rate)
+                start, stop = xlim  # units: samples
                 self.max_amplitudes_syn = np.append(
                         self.max_amplitudes_syn, max(abs(tr.data[start:stop]))
                         )
@@ -939,7 +932,7 @@ class RecordSection:
                                    f"length of data trace")
                     end_index = len(tr.data)
 
-                xlim.append((start_index, end_index))
+                xlim.append((int(start_index), int(end_index)))
 
         xlim = np.array(xlim)
         return xlim